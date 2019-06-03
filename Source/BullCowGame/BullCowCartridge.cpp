d4d054afdc1a7574ed8492e5df5af1f8ec992495
Reina Liew <notalk_0@hotmail.com>
2019-06-03 13:33:46 +0800

    Initial Commit

---
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine("Welcome to Bulls & Cows");
+}
+
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
+{
+    ClearScreen();
+}
